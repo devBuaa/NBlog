9841cb12d471cdf850ee7b0e4c0d6cddb0e19fe7
hsu <hsu@DESKTOP-AF1DL34>
2016-08-02 18:25:38 +0800

    add rapid generator

---
+++ NBlog/src/generator/nblog/Generator.java
@@ -0,0 +1,19 @@
+package generator.nblog;
+
+import cn.org.rapid_framework.generator.GeneratorFacade;
+
+public class Generator {
+
+    public static void main(String[] args) throws Exception {
+        // TODO Auto-generated method stub
+        GeneratorFacade g = new GeneratorFacade();
+        g.generateByTable("Test","template");
+        //通过数据库表生成文件,template为模板的根目录
+        //g.generateByAllTable("template");
+        //自动搜索数据库中的所有表并生成文件,template为模板的根目录
+        //g.generateByClass(Blog.class,"template_clazz");
+        //  g.deleteByTable("table_name", "template");
+        //删除生成的文件
+    }
+
+}
